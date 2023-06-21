3c7b469866020cbdaf5f431fcdd9a91039d4f77d
JohnDeveloper <johnisthedeveloper@gmail.com>
2023-06-20 21:48:50 -0400

    commit

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Nomad AI - Before die, I do';
+export const SITE_DESCRIPTION = '人工智能游牧时代';
